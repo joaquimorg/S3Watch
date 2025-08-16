a070e168e10d00b645c2ba79448e19baca1667a8
joaquim.org <joaquim.org@gmail.com>
2025-08-16 21:43:32 +0100

    fix build

--- components/nimble-nordic-uart/src/nimble.c
+++ components/nimble-nordic-uart/src/nimble.c
@@ -46,26 +46,26 @@ static void (*_nordic_uart_callback)(enum nordic_uart_callback_type callback_typ
 static uart_receive_callback_t _uart_receive_callback = NULL;
 
 esp_err_t nordic_uart_yield(uart_receive_callback_t uart_receive_callback) {
-  _uart_receive_callback = uart_receive_callback;
-  return ESP_OK;
+    _uart_receive_callback = uart_receive_callback;
+    return ESP_OK;
 }
 
 static int _uart_receive(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt* ctxt, void* arg) {
-  if (_uart_receive_callback) {
-    _uart_receive_callback(ctxt);
-  }
-  else {
-    for (int i = 0; i < ctxt->om->om_len; ++i) {
-      const char c = ctxt->om->om_data[i];
-      _nordic_uart_linebuf_append(c);
+    if (_uart_receive_callback) {
+        _uart_receive_callback(ctxt);
     }
-  }
-  return 0;
+    else {
+        for (int i = 0; i < ctxt->om->om_len; ++i) {
+            const char c = ctxt->om->om_data[i];
+            _nordic_uart_linebuf_append(c);
+        }
+    }
+    return 0;
 }
 
 // notify GATT callback is no operation.
 static int _uart_noop(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt* ctxt, void* arg) {
-  return 0;
+    return 0;
 }
 
 static const struct ble_gatt_svc_def gat_svcs[] = {
@@ -93,156 +93,156 @@ static const struct ble_gatt_svc_def gat_svcs[] = {
 static int ble_gap_event_cb(struct ble_gap_event* event, void* arg);
 
 static void ble_app_advertise(void) {
-  struct ble_hs_adv_fields fields, fields_ext;
-  memset(&fields, 0, sizeof(fields));
+    struct ble_hs_adv_fields fields, fields_ext;
+    memset(&fields, 0, sizeof(fields));
 
-  // fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_DISC_LTD;
-  fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP;
+    // fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_DISC_LTD;
+    fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP;
 
-  fields.tx_pwr_lvl_is_present = 1;
-  fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+    fields.tx_pwr_lvl_is_present = 1;
+    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
 
-  const char* name = ble_svc_gap_device_name();
-  /*fields.name = (uint8_t*)name;
-  fields.name_len = strlen(name);
-  fields.name_is_complete = 1;*/
+    const char* name = ble_svc_gap_device_name();
+    /*fields.name = (uint8_t*)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;*/
 
-  fields.uuids128_is_complete = 1;
-  fields.uuids128 = &SERVICE_UUID;
-  fields.num_uuids128 = 1;
+    fields.uuids128_is_complete = 1;
+    fields.uuids128 = &SERVICE_UUID;
+    fields.num_uuids128 = 1;
 
-  int err = ble_gap_adv_set_fields(&fields);
-  if (err) {
-    ESP_LOGE(_TAG, "ble_gap_adv_set_fields, err %d", err);
-  }
+    int err = ble_gap_adv_set_fields(&fields);
+    if (err) {
+        ESP_LOGE(_TAG, "ble_gap_adv_set_fields, err %d", err);
+    }
 
-  memset(&fields_ext, 0, sizeof(fields_ext));
-  fields_ext.flags = fields.flags;
-  fields_ext.name = (uint8_t*)name;
-  fields_ext.name_len = strlen(name);
-  fields_ext.name_is_complete = 1;
-  err = ble_gap_adv_rsp_set_fields(&fields_ext);
-  if (err) {
-    ESP_LOGE(_TAG, "ble_gap_adv_rsp_set_fields fields_ext, name might be too long, err %d", err);
-  }
+    memset(&fields_ext, 0, sizeof(fields_ext));
+    fields_ext.flags = fields.flags;
+    fields_ext.name = (uint8_t*)name;
+    fields_ext.name_len = strlen(name);
+    fields_ext.name_is_complete = 1;
+    err = ble_gap_adv_rsp_set_fields(&fields_ext);
+    if (err) {
+        ESP_LOGE(_TAG, "ble_gap_adv_rsp_set_fields fields_ext, name might be too long, err %d", err);
+    }
 
-  struct ble_gap_adv_params adv_params;
-  memset(&adv_params, 0, sizeof(adv_params));
-  adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
-  adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    struct ble_gap_adv_params adv_params;
+    memset(&adv_params, 0, sizeof(adv_params));
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
 
-  err = ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event_cb, NULL);
-  if (err) {
-    ESP_LOGE(_TAG, "Advertising start failed: err %d", err);
-  }
+    err = ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event_cb, NULL);
+    if (err) {
+        ESP_LOGE(_TAG, "Advertising start failed: err %d", err);
+    }
 }
 
 static int ble_gap_event_cb(struct ble_gap_event* event, void* arg) {
-  switch (event->type) {
-  case BLE_GAP_EVENT_CONNECT:
-    ESP_LOGI(_TAG, "BLE_GAP_EVENT_CONNECT %s", event->connect.status == 0 ? "OK" : "Failed");
-    if (event->connect.status == 0) {
-      ble_conn_hdl = event->connect.conn_handle;
-      /* Check connection handle */
-      struct ble_gap_conn_desc desc;
-      int rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
-      if (rc != 0) {
-        ESP_LOGE(_TAG,
-          "failed to find connection by handle, error code: %d",
-          rc);
-        return rc;
-      }
+    switch (event->type) {
+    case BLE_GAP_EVENT_CONNECT:
+        ESP_LOGI(_TAG, "BLE_GAP_EVENT_CONNECT %s", event->connect.status == 0 ? "OK" : "Failed");
+        if (event->connect.status == 0) {
+            ble_conn_hdl = event->connect.conn_handle;
+            /* Check connection handle */
+            struct ble_gap_conn_desc desc;
+            int rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+            if (rc != 0) {
+                ESP_LOGE(_TAG,
+                    "failed to find connection by handle, error code: %d",
+                    rc);
+                return rc;
+            }
 
-      /* Try to update connection parameters! */
-      struct ble_gap_upd_params params = { .itvl_min = desc.conn_itvl,
-                                          .itvl_max = desc.conn_itvl,
-                                          .latency = 3,
-                                          .supervision_timeout =
-                                              desc.supervision_timeout };
-      rc = ble_gap_update_params(event->connect.conn_handle, &params);
-      if (rc != 0) {
-        ESP_LOGE(
-          _TAG,
-          "failed to update connection parameters, error code: %d",
-          rc);
-        return rc;
-      }
-      if (_nordic_uart_callback)
-        _nordic_uart_callback(NORDIC_UART_CONNECTED);
+            /* Try to update connection parameters! */
+            struct ble_gap_upd_params params = { .itvl_min = desc.conn_itvl,
+                                                .itvl_max = desc.conn_itvl,
+                                                .latency = 3,
+                                                .supervision_timeout =
+                                                    desc.supervision_timeout };
+            rc = ble_gap_update_params(event->connect.conn_handle, &params);
+            if (rc != 0) {
+                ESP_LOGE(
+                    _TAG,
+                    "failed to update connection parameters, error code: %d",
+                    rc);
+                return rc;
+            }
+            if (_nordic_uart_callback)
+                _nordic_uart_callback(NORDIC_UART_CONNECTED);
+        }
+        else {
+            ble_app_advertise();
+        }
+        break;
+    case BLE_GAP_EVENT_DISCONNECT:
+        _nordic_uart_linebuf_append('\003'); // send Ctrl-C
+        ESP_LOGI(_TAG, "BLE_GAP_EVENT_DISCONNECT");
+        if (_nordic_uart_callback)
+            _nordic_uart_callback(NORDIC_UART_DISCONNECTED);
+        ble_app_advertise();
+        break;
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        ESP_LOGI(_TAG, "BLE_GAP_EVENT_ADV_COMPLETE");
+        ble_app_advertise();
+        break;
+    case BLE_GAP_EVENT_SUBSCRIBE:
+        if (event->subscribe.attr_handle == notify_char_attr_hdl) {
+            if (event->subscribe.cur_notify == 0) {
+                ESP_LOGI(_TAG, "Client unsubscribed from notifications");
+            }
+            else {
+                ESP_LOGI(_TAG, "Client subscribed to notifications");
+            }
+        }
+        else {
+            ESP_LOGW(_TAG, "Unknown subscribe event for attr_handle %d", event->subscribe.attr_handle);
+        }
+        ESP_LOGI(_TAG, "BLE_GAP_EVENT_SUBSCRIBE");
+        break;
+    default:
+        break;
     }
-    else {
-      ble_app_advertise();
-    }
-    break;
-  case BLE_GAP_EVENT_DISCONNECT:
-    _nordic_uart_linebuf_append('\003'); // send Ctrl-C
-    ESP_LOGI(_TAG, "BLE_GAP_EVENT_DISCONNECT");
-    if (_nordic_uart_callback)
-      _nordic_uart_callback(NORDIC_UART_DISCONNECTED);
-    ble_app_advertise();
-    break;
-  case BLE_GAP_EVENT_ADV_COMPLETE:
-    ESP_LOGI(_TAG, "BLE_GAP_EVENT_ADV_COMPLETE");
-    ble_app_advertise();
-    break;
-  case BLE_GAP_EVENT_SUBSCRIBE:
-    if (event->subscribe.attr_handle == notify_char_attr_hdl) {
-      if (event->subscribe.cur_notify == 0) {
-        ESP_LOGI(_TAG, "Client unsubscribed from notifications");
-      }
-      else {
-        ESP_LOGI(_TAG, "Client subscribed to notifications");
-      }
-    }
-    else {
-      ESP_LOGW(_TAG, "Unknown subscribe event for attr_handle %d", event->subscribe.attr_handle);
-    }
-    ESP_LOGI(_TAG, "BLE_GAP_EVENT_SUBSCRIBE");
-    break;
-  default:
-    break;
-  }
-  return 0;
+    return 0;
 }
 
 static void ble_app_on_sync_cb(void) {
-  int ret = ble_hs_id_infer_auto(0, &ble_addr_type);
-  if (ret != 0) {
-    ESP_LOGE(_TAG, "Error ble_hs_id_infer_auto: %d", ret);
-  }
-  ble_app_advertise();
+    int ret = ble_hs_id_infer_auto(0, &ble_addr_type);
+    if (ret != 0) {
+        ESP_LOGE(_TAG, "Error ble_hs_id_infer_auto: %d", ret);
+    }
+    ble_app_advertise();
 }
 
 // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/bluetooth/nimble/index.html#_CPPv434esp_nimble_hci_and_controller_initv
 static void ble_host_task(void* param) {
-  ESP_LOGI(_TAG, "BLE Host Task Started");
-  nimble_port_run(); // This function will return only when nimble_port_stop() is executed.
-  nimble_port_freertos_deinit();
-  _nordic_uart_buf_deinit();
+    ESP_LOGI(_TAG, "BLE Host Task Started");
+    nimble_port_run(); // This function will return only when nimble_port_stop() is executed.
+    nimble_port_freertos_deinit();
+    _nordic_uart_buf_deinit();
 }
 
 // Split the message in BLE_SEND_MTU and send it.
 esp_err_t _nordic_uart_send(const char* message) {
-  const int len = strlen(message);
-  if (len == 0)
-    return ESP_OK;
-  // Split the message in BLE_SEND_MTU and send it.
-  for (int i = 0; i < len; i += BLE_SEND_MTU) {
-    int err;
-    struct os_mbuf* om;
-    int err_count = 0;
-  do_notify:
-    om = ble_hs_mbuf_from_flat(&message[i], MIN(BLE_SEND_MTU, len - i));
-    //err = ble_gattc_notify_custom(ble_conn_hdl, notify_char_attr_hdl, om);
-    err = ble_gatts_notify_custom(ble_conn_hdl, notify_char_attr_hdl, om);
-    if (err == BLE_HS_ENOMEM && err_count++ < 10) {
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      goto do_notify;
+    const int len = strlen(message);
+    if (len == 0)
+        return ESP_OK;
+    // Split the message in BLE_SEND_MTU and send it.
+    for (int i = 0; i < len; i += BLE_SEND_MTU) {
+        int err;
+        struct os_mbuf* om;
+        int err_count = 0;
+    do_notify:
+        om = ble_hs_mbuf_from_flat(&message[i], MIN(BLE_SEND_MTU, len - i));
+        //err = ble_gattc_notify_custom(ble_conn_hdl, notify_char_attr_hdl, om);
+        err = ble_gatts_notify_custom(ble_conn_hdl, notify_char_attr_hdl, om);
+        if (err == BLE_HS_ENOMEM && err_count++ < 10) {
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            goto do_notify;
+        }
+        if (err)
+            return ESP_FAIL;
     }
-    if (err)
-      return ESP_FAIL;
-  }
-  return ESP_OK;
+    return ESP_OK;
 }
 
 /***
@@ -252,82 +252,82 @@ esp_err_t _nordic_uart_send(const char* message) {
  */
 esp_err_t _nordic_uart_start(const char* device_name, void (*callback)(enum nordic_uart_callback_type callback_type)) {
 
-  int rc;
+    int rc;
 
-  // already initialized will return ESP_FAIL
-  if (_nordic_uart_linebuf_initialized()) {
-    ESP_LOGE(_TAG, "Already initialized");
-    return ESP_FAIL;
-  }
+    // already initialized will return ESP_FAIL
+    if (_nordic_uart_linebuf_initialized()) {
+        ESP_LOGE(_TAG, "Already initialized");
+        return ESP_FAIL;
+    }
 
-  if (nvs_flash_init() != ESP_OK) {
-    ESP_LOGE(_TAG, "Failed to nvs_flash_init");
-    return ESP_FAIL;
-  }
+    if (nvs_flash_init() != ESP_OK) {
+        ESP_LOGE(_TAG, "Failed to nvs_flash_init");
+        return ESP_FAIL;
+    }
 
-  _nordic_uart_callback = callback;
-  _nordic_uart_buf_init();
+    _nordic_uart_callback = callback;
+    _nordic_uart_buf_init();
 
-  // Initialize controller and NimBLE host
-  esp_err_t ret = esp_nimble_hci_and_controller_init();
-  if (ret != ESP_OK) {
-    ESP_LOGE(_TAG, "esp_nimble_hci_and_controller_init() failed with error: %d", ret);
-    return ESP_FAIL;
-  }
+    // Initialize controller and NimBLE host
+    esp_err_t ret = esp_nimble_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(_TAG, "esp_nimble_init() failed with error: %d", ret);
+        return ESP_FAIL;
+    }
 
-  ret = nimble_port_init();
-  if (ret != ESP_OK) {
-    ESP_LOGE(_TAG, "nimble_port_init() failed with error: %d", ret);
-    esp_nimble_hci_and_controller_deinit();
-    return ESP_FAIL;
-  }
+    ret = nimble_port_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(_TAG, "nimble_port_init() failed with error: %d", ret);
+        esp_nimble_deinit();
+        return ESP_FAIL;
+    }
 
-  // Initialize the NimBLE Host configuration
-  // Bluetooth device name for advertisement
+    // Initialize the NimBLE Host configuration
+    // Bluetooth device name for advertisement
 
-  ble_hs_cfg.sync_cb = ble_app_on_sync_cb;
+    ble_hs_cfg.sync_cb = ble_app_on_sync_cb;
 
-  ble_svc_gap_init();
-  ble_svc_gatt_init();
+    ble_svc_gap_init();
+    ble_svc_gatt_init();
 
-  rc = ble_gatts_count_cfg(gat_svcs);
-  assert(rc == 0);
+    rc = ble_gatts_count_cfg(gat_svcs);
+    assert(rc == 0);
 
-  rc = ble_gatts_add_svcs(gat_svcs);
-  assert(rc == 0);
+    rc = ble_gatts_add_svcs(gat_svcs);
+    assert(rc == 0);
 
-  /* Set the default device name */
-  rc = ble_svc_gap_device_name_set(device_name);
-  assert(rc == 0);
+    /* Set the default device name */
+    rc = ble_svc_gap_device_name_set(device_name);
+    assert(rc == 0);
 
 
-  // Create NimBLE thread
-  nimble_port_freertos_init(ble_host_task);
+    // Create NimBLE thread
+    nimble_port_freertos_init(ble_host_task);
 
-  return ESP_OK;
+    return ESP_OK;
 }
 
 esp_err_t _nordic_uart_stop(void) {
-  esp_err_t rc = ble_gap_adv_stop();
-  if (rc) {
-    // if already stoped BLE, some error is raised. but no problem.
-    ESP_LOGD(_TAG, "Error in stopping advertisement with err code = %d", rc);
-    return ESP_FAIL;
+    esp_err_t rc = ble_gap_adv_stop();
+    if (rc) {
+        // if already stoped BLE, some error is raised. but no problem.
+        ESP_LOGD(_TAG, "Error in stopping advertisement with err code = %d", rc);
+        return ESP_FAIL;
 
-  }
-
-  int ret = nimble_port_stop();
-  if (ret == ESP_OK) {
-    ret = nimble_port_deinit();
-    if (ret != ESP_OK) {
-      ESP_LOGE(_TAG, "nimble_port_deinit() failed with error: %d", ret);
-      return ESP_FAIL;
     }
-  }
-  esp_nimble_hci_and_controller_deinit();
-  _nordic_uart_buf_deinit();
 
-  _nordic_uart_callback = NULL;
+    int ret = nimble_port_stop();
+    if (ret == ESP_OK) {
+        ret = nimble_port_deinit();
+        if (ret != ESP_OK) {
+            ESP_LOGE(_TAG, "nimble_port_deinit() failed with error: %d", ret);
+            return ESP_FAIL;
+        }
+    }
+    esp_nimble_deinit();
+    _nordic_uart_buf_deinit();
 
-  return ESP_OK;
+    _nordic_uart_callback = NULL;
+
+    return ESP_OK;
 }

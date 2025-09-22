6982b30b79b1285346d11470eab1ccf454bb9890
joaquim.org <joaquim.org@gmail.com>
2025-09-22 22:59:38 +0100

    Add time display functionality to settings screen with timer management

--- components/gui/src/settings_screen.c
+++ components/gui/src/settings_screen.c
@@ -12,6 +12,7 @@
 
 #include "ui.h"
 #include "watchface.h"
+#include "bsp/esp32_s3_touch_amoled_2_06.h"
 
 static const char* TAG = "SETTINGS SCREEN";
 
@@ -27,8 +28,13 @@ LV_IMAGE_DECLARE(image_settings_icon);
 
 static void click_event_cb(lv_event_t* e);
 static void toggle_event_cb(lv_event_t* e);
+static void time_timer_cb(lv_timer_t* timer);
+static void update_time_label(void);
+static void control_screen_on_delete(lv_event_t* e);
 
 static lv_obj_t* control_screen;
+static lv_obj_t* time_label;
+static lv_timer_t* time_timer;
 
 static const lv_image_dsc_t* control_icons[] = {
     &image_brightness_icon,
@@ -57,7 +63,53 @@ enum control_id {
     CTRL_SETTINGS,
 };
 
+static void update_time_label(void)
+{
+    if (!time_label) {
+        return;
+    }
+    lv_label_set_text_fmt(time_label, "%02d:%02d", rtc_get_hour(), rtc_get_minute());
+}
+
+static void time_timer_cb(lv_timer_t* timer)
+{
+    (void)timer;
+    bool locked = bsp_display_lock(0);
+    update_time_label();
+    if (locked) {
+        bsp_display_unlock();
+    }
+}
+
+static void control_screen_on_delete(lv_event_t* e)
+{
+    (void)e;
+    if (time_timer) {
+        lv_timer_del(time_timer);
+        time_timer = NULL;
+    }
+    time_label = NULL;
+    control_screen = NULL;
+}
+
 static void screen_events(lv_event_t* e);
+
+static void screen_events(lv_event_t* e)
+{
+    if (lv_event_get_code(e) == LV_EVENT_GESTURE) {
+        lv_dir_t dir = lv_indev_get_gesture_dir(lv_indev_active());
+        if (dir == LV_DIR_BOTTOM) {
+            lv_indev_wait_release(lv_indev_active());
+            load_screen(control_screen, watchface_screen_get(), LV_SCR_LOAD_ANIM_MOVE_BOTTOM);
+        }
+    }
+    else if (lv_event_get_code(e) == LV_EVENT_SCREEN_LOADED) {
+        update_time_label();
+        if (time_timer) {
+            lv_timer_ready(time_timer);
+        }
+    }
+}
 
 void control_screen_create(lv_obj_t* parent)
 {
@@ -69,43 +121,54 @@ void control_screen_create(lv_obj_t* parent)
     lv_style_set_text_color(&cmain_style, lv_color_white());
     lv_style_set_bg_color(&cmain_style, lv_color_hex(0x000000));
     lv_style_set_bg_opa(&cmain_style, LV_OPA_100);
-    //lv_style_set_radius(&cmain_style, LV_RADIUS_CIRCLE);
-    //lv_style_set_translate_y(&cmain_style, -SCREEN_SIZE);
-
-    lv_style_set_layout(&cmain_style, LV_LAYOUT_FLEX);
-    lv_style_set_flex_flow(&cmain_style, LV_FLEX_FLOW_ROW_WRAP);
-    lv_style_set_flex_main_place(&cmain_style, LV_FLEX_ALIGN_CENTER);
-    lv_style_set_flex_cross_place(&cmain_style, LV_FLEX_ALIGN_CENTER);
-    lv_style_set_flex_track_place(&cmain_style, LV_FLEX_ALIGN_CENTER);
-    lv_style_set_pad_row(&cmain_style, 14);
-    lv_style_set_pad_column(&cmain_style, 14);
-    //lv_style_set_pad_top(&cmain_style, 25);
-
 
     control_screen = lv_obj_create(parent);
     lv_obj_remove_style_all(control_screen);
     lv_obj_add_style(control_screen, &cmain_style, 0);
     lv_obj_set_size(control_screen, lv_pct(100), lv_pct(100));
     lv_obj_center(control_screen);
-    //lv_obj_remove_flag(control_screen, LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_EVENT_BUBBLE);
     lv_obj_clear_flag(control_screen, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_flex_flow(control_screen, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_align(control_screen, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    lv_obj_set_style_pad_row(control_screen, 12, 0);
     lv_obj_add_event_cb(control_screen, screen_events, LV_EVENT_GESTURE, NULL);
+    lv_obj_add_event_cb(control_screen, screen_events, LV_EVENT_SCREEN_LOADED, NULL);
+    lv_obj_add_event_cb(control_screen, control_screen_on_delete, LV_EVENT_DELETE, NULL);
 
-    // Header
-    /*lv_obj_t* hdr = lv_obj_create(control_screen);
-    lv_obj_remove_style_all(hdr);
-    lv_obj_set_size(hdr, lv_pct(100), LV_SIZE_CONTENT);
-    lv_obj_set_flex_flow(hdr, LV_FLEX_FLOW_ROW);
-    lv_obj_set_flex_align(hdr, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
+    lv_obj_t* header = lv_obj_create(control_screen);
+    lv_obj_remove_style_all(header);
+    lv_obj_clear_flag(header, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(header, lv_pct(100), LV_SIZE_CONTENT);
+    //lv_obj_set_style_pad_left(header, 20, 0);
+    //lv_obj_set_style_pad_right(header, 20, 0);
+    //lv_obj_set_style_pad_top(header, 10, 0);
+    //lv_obj_set_style_pad_bottom(header, 6, 0);
+    lv_obj_set_style_bg_opa(header, LV_OPA_TRANSP, 0);
+    lv_obj_set_flex_flow(header, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(header, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
-    lv_obj_t* title = lv_label_create(hdr);
-    lv_obj_set_style_text_font(title, &font_normal_26, 0);
-    lv_label_set_text_fmt(title, "%02d:%02d", rtc_get_hour(), rtc_get_minute());
-    lv_obj_set_style_pad_top(title, 6, 0);*/
+    time_label = lv_label_create(header);
+    lv_obj_set_style_text_font(time_label, &font_bold_32, 0);
+    lv_obj_set_style_text_color(time_label, lv_color_white(), 0);
+    lv_label_set_text(time_label, "--:--");
+
+    lv_obj_t* grid = lv_obj_create(control_screen);
+    lv_obj_remove_style_all(grid);
+    lv_obj_clear_flag(grid, LV_OBJ_FLAG_SCROLLABLE);
+    //lv_obj_set_width(grid, lv_pct(100));
+    lv_obj_set_size(grid, lv_pct(100), LV_SIZE_CONTENT);
+    lv_obj_set_style_pad_top(grid, 10, 0);
+    lv_obj_set_style_pad_left(grid, 12, 0);
+    lv_obj_set_style_pad_right(grid, 12, 0);
+    lv_obj_set_style_pad_row(grid, 14, 0);
+    lv_obj_set_style_pad_column(grid, 14, 0);
+    lv_obj_set_style_bg_opa(grid, LV_OPA_TRANSP, 0);
+    lv_obj_set_flex_flow(grid, LV_FLEX_FLOW_ROW_WRAP);
+    lv_obj_set_flex_align(grid, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
     /* Create grid-like items (flex row wrap, non-scrollable) */
     for (uint32_t i = 0; i < 6; i++) {
-        lv_obj_t* item = lv_obj_create(control_screen);
+        lv_obj_t* item = lv_obj_create(grid);
         lv_obj_remove_style_all(item);
         lv_obj_set_width(item, lv_pct(46));
         lv_obj_set_height(item, 110);
@@ -127,14 +190,19 @@ void control_screen_create(lv_obj_t* parent)
             if (i == CTRL_SILENCE) {
                 bool sound_on = settings_get_sound();
                 bool silent = !sound_on;
-                if (silent) lv_obj_add_state(item, LV_STATE_CHECKED);
-                else lv_obj_clear_state(item, LV_STATE_CHECKED);
+                if (silent) {
+                    lv_obj_add_state(item, LV_STATE_CHECKED);
+                }
+                else {
+                    lv_obj_clear_state(item, LV_STATE_CHECKED);
+                }
             }
             if (i == CTRL_BLUETOOTH) {
                 bool ble_on = ble_sync_is_enabled();
                 if (ble_on) {
                     lv_obj_add_state(item, LV_STATE_CHECKED);
-                } else {
+                }
+                else {
                     lv_obj_clear_state(item, LV_STATE_CHECKED);
                 }
             }
@@ -155,19 +223,10 @@ void control_screen_create(lv_obj_t* parent)
         lv_obj_set_style_text_font(label, &font_normal_26, 0);
     }
 
-}
-
-static void screen_events(lv_event_t* e)
-{
-    if (lv_event_get_code(e) == LV_EVENT_GESTURE) {
-        lv_dir_t dir = lv_indev_get_gesture_dir(lv_indev_active());
-        if (dir == LV_DIR_BOTTOM) {
-            lv_indev_wait_release(lv_indev_active());
-            load_screen(control_screen, watchface_screen_get(), LV_SCR_LOAD_ANIM_MOVE_BOTTOM);
-        }
-    }
-    else if (lv_event_get_code(e) == LV_EVENT_SCREEN_LOADED) {
-        
+    update_time_label();
+    if (!time_timer) {
+        time_timer = lv_timer_create(time_timer_cb, 1000, NULL);
+        lv_timer_ready(time_timer);
     }
 }
 
